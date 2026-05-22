b95926a57f6f620be51e4bd20c8ebe814abc8d69
Victor García-Carpintero Burgos <victor.gcb@gmail.com>
2026-05-22 10:53:45 +0200

    Update run.py

--- src/run.py
+++ src/run.py
@@ -138,6 +138,7 @@ def run_interpro(sequence, threads):
             msg = "InterProScan ran succesfully"
         else:
             msg = run_.stdout.decode()
+    print(cmd)
     interpro_results = {"command": cmd, "msg": msg,
                         "out_fpath": out_fpath, "returncode": returncode}
         
